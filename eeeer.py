6d300672d2fb186ab5b5f4b85935ede1105de1c9
Engineer <arsenya.suvorov@mail.ru>
2023-07-14 17:18:02 +0400

    Start

--- eeeer.py
+++ eeeer.py
@@ -5,7 +5,7 @@ excel_data = pd.read_excel('test.xlsx')
 
 data = pd.DataFrame(excel_data, columns=['name', 'inn', 'debit_date','debit_balance','guarantee','total'])
 
-print("The content of the file is:\n", data)
+# print("The content of the file is:\n", data)
 
 h = data.count()[0]
 n = 0
@@ -17,5 +17,5 @@ for i in range(h):
         data.at [n, 'total'] = 'Проблемная задолженность в сумме ' + str(data.iloc[n,3] ) + 'рублей признана безнадежной и списана на внебалансовые счета ' + str(data.iloc[n,2] )
         n += 1
 
-print(data)
+# print(data)
 data.to_excel('test.xlsx')
